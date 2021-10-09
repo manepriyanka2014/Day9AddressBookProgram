9c252c94cd45a7c39abd8e557aaa62f23fc0b92e
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-09 22:15:15 +0530

    Create Contact

---
+++ AddressBookUC1.java
@@ -0,0 +1,21 @@
+package com.addressbook;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBookUC1 {
+
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private long zip;
+	private long phoneNumber;
+	private String email;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address book Program");
+
+	}
+
+}
